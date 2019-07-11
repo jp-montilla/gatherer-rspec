dd0133059d7608f4903aa50af60c9419143e63a2
John Paul Montilla <johnpaul.montilla1315@gmail.com>
2019-07-11 14:49:17 +0800

    Test 1

---
+++ app/models/project.rb
@@ -0,0 +1,32 @@
+class Project
+  attr_accessor :tasks, :due_date
+  
+  def initialize 
+    @tasks = []
+  end
+  def done? 
+    tasks.all?(&:complete?)
+  end
+  def total_size 
+    tasks.sum(&:size)
+  end
+  def remaining_size 
+    tasks.reject(&:complete?).sum(&:size)
+  end
+
+  def completed_velocity 
+    tasks.sum(&:points_toward_velocity)
+  end
+  def current_rate 
+    completed_velocity * 1.0 / 21
+  end
+  def projected_days_remaining 
+    remaining_size / current_rate
+  end
+  def on_schedule?
+    (Time.zone.today + projected_days_remaining) <= due_date
+  end
+
+
+
+end
---
+++ spec/models/project_spec.rb
@@ -0,0 +1,53 @@
+require "rails_helper"
+
+RSpec.describe Project do 
+  let(:project) { Project.new } 
+  let(:task) { Task.new }
+
+  it "marks a project done if its tasks are done" do 
+    project.tasks << task
+    task.mark_completed
+    expect(project).to be_done
+  end
+  
+end
+
+RSpec.describe "estimates" do
+  
+  let(:project) { Project.new }
+  let(:newly_done) { Task.new(size: 3, completed_at: 1.day.ago) } 
+  let(:old_done) { Task.new(size: 2, completed_at: 6.months.ago) } 
+  let(:small_not_done) { Task.new(size: 1) }
+  let(:large_not_done) { Task.new(size: 4) }
+
+  before(:example) do
+    project.tasks = [newly_done, old_done, small_not_done, large_not_done]
+  end
+  it "can calculate total size" do
+      expect(project.total_size).to eq(10)
+  end
+  it "can calculate remaining size" do
+      expect(project.remaining_size).to eq(5)
+  end 
+
+  it "knows its velocity" do 
+    expect(project.completed_velocity).to eq(3)
+  end
+  it "knows its rate" do 
+    expect(project.current_rate).to eq(1.0 / 7)
+  end
+  it "knows its projected days remaining" do 
+    expect(project.projected_days_remaining).to eq(35)
+  end
+  it "knows if it is not on schedule" do 
+    project.due_date = 1.week.from_now 
+    expect(project).not_to be_on_schedule
+  end
+  it "knows if it is on schedule" do 
+    project.due_date = 6.months.from_now 
+    expect(project).to be_on_schedule
+  end
+end
+
+
+
